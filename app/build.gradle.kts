9107f2db0d4d0f0c71b127b9bfbd355c46e3bebe
Уля <ulya@Ulianas-MarkBook.local>
2026-07-31 16:45:15 +0200

    Prepare 0.10.3 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mark.babylog"
         minSdk = 26
         targetSdk = 35
-        versionCode = 38
-        versionName = "0.10.2"
+        versionCode = 39
+        versionName = "0.10.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
